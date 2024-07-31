03d5d1d80bbca3076f10e78aa48b0dcd0025e4a2
Lelia <lambmaan@gmail.com>
2024-07-31 14:04:19 +0100

    idk

---
+++ finalProjectQuizPage/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  finalProjectQuizPage
+//
+//  Created by Scholar on 31/07/2024.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
